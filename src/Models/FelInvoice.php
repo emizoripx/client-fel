2d4b8ceb43ebcf3dd7a5a22a509159028e7c98ae
Marco Cuellar <mcuellar@ipxserver.com>
2021-01-26 11:24:41 -0400

    added cast columns in model

--- src/Models/FelInvoice.php
+++ src/Models/FelInvoice.php
@@ -8,4 +8,20 @@ class FelInvoice extends Model {
 
 
     protected $guarded =[];
+
+    protected $casts = [
+        'updated_at' => 'timestamp',
+        'created_at' => 'timestamp',
+        'deleted_at' => 'timestamp',
+        'sucursal' => 'object',
+        'documentoIdentidad'  => 'object',
+        'metodoPago' => 'object',
+        'moneda' => 'object',
+        'documentoSector' => 'object',
+        'extras' => 'object',
+        'errores' => 'object',
+        'detalle' => 'object'
+    ];
+
+
 }
