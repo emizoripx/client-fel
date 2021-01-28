90bf607166b58ca44a2975ee449284d207b51fd4
Marco Cuellar <mcuellar@ipxserver.com>
2021-01-28 11:38:35 -0400

    adding fillable a cast fields for invoice

--- src/Models/FelInvoice.php
+++ src/Models/FelInvoice.php
@@ -6,22 +6,52 @@ use Illuminate\Database\Eloquent\Model;
 
 class FelInvoice extends Model {
 
+    protected $table = 'fel_invoices';
 
-    protected $guarded =[];
+    protected $fillable =[
+        'estado',
+        'codigoRecepcion',
+        'codigoEstado',
+        'tipEmision',
+        'nitEmisor',
+        'numeroFactura',
+        'cuf',
+        'cufd',
+        'sucursal',
+        'direccion',
+        'codigoPuntoVenta',
+        'fechaEmision',
+        'nombreRazonSocial',
+        'documentoIdentidad',
+        'numeroDocumento',
+        'complemento',
+        'codigoCliente',
+        'metodoPago',
+        'numeroTarjeta',
+        'montoTotal',
+        'moneda',
+        'montoTotalMoneda',
+        'leyenda',
+        'documentoSector',
+        'extras',
+        'pdf_url',
+        'errores',
+        'montoTotalSujetoIva',
+        'tipoCambio',
+        'detalle'
+    ];
 
     protected $casts = [
         'updated_at' => 'timestamp',
         'created_at' => 'timestamp',
         'deleted_at' => 'timestamp',
-        'sucursal' => 'object',
-        'documentoIdentidad'  => 'object',
-        'metodoPago' => 'object',
-        'moneda' => 'object',
-        'documentoSector' => 'object',
-        'extras' => 'object',
-        'errores' => 'object',
-        'detalle' => 'object'
+        'sucursal' => 'json',
+        'documentoIdentidad'  => 'json',
+        'metodoPago' => 'json',
+        'moneda' => 'json',
+        'documentoSector' => 'json',
+        'extras' => 'json',
+        'errores' => 'json',
+        'detalle' => 'json'
     ];
-
-
 }
