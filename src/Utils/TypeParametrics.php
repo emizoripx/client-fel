48c8cce581ba309f52b7fcc01f2500472ae99562
Marco Cuellar <mcuellar@ipxserver.com>
2021-02-09 14:57:17 -0400

    fix name of endpoint motivos de anulacion typeoff

--- src/Utils/TypeParametrics.php
+++ src/Utils/TypeParametrics.php
@@ -4,7 +4,7 @@ namespace EmizorIpx\ClientFel\Utils;
 
 class TypeParametrics {
 
-    const MOTIVO_ANULACION="motivos-anulacion";
+    const MOTIVO_ANULACION="motivos-de-anulacion";
     const PAISES="paises";
     const TIPOS_DOCUMENTO_IDENTIDAD="tipos-documento-de-identidad";
     const METODOS_DE_PAGO="metodos-de-pago";
