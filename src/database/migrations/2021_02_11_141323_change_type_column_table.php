f7d89536a126d6e1968977e82e15d273d9dd9e1c
Marco Cuellar <mcuellar@ipxserver.com>
2021-02-11 10:18:49 -0400

    change type column and return format fel_data for parametrics

---
+++ src/database/migrations/2021_02_11_141323_change_type_column_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeTypeColumnTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        DB::statement("truncate fel_captions");
+        // Leyendas de factura
+        Schema::create('fel_captions', function (Blueprint $table) {
+            $table->unsignedInteger('codigo')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
