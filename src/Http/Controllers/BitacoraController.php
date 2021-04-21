71a5abb9a2b1bcec24d07098a255f22f0faf857d
Marco Cuellar <mcuellar@ipxserver.com>
2021-04-21 19:43:34 -0400

    add method to update tokens

--- src/Http/Controllers/BitacoraController.php
+++ src/Http/Controllers/BitacoraController.php
@@ -4,7 +4,9 @@ namespace EmizorIpx\ClientFel\Http\Controllers;
 
 use App\Http\Controllers\BaseController;
 use EmizorIpx\ClientFel\Models\BitacoraLog;
+use EmizorIpx\ClientFel\Models\FelClientToken;
 use Illuminate\Http\Request;
+use EmizorIpx\ClientFel\Services\Connection\Connection;
 
 class BitacoraController extends BaseController
 {
@@ -15,5 +17,24 @@ class BitacoraController extends BaseController
 
         return view('clientfel::bitacora', compact('logs') );
         
+    }
+
+    public function updateTokens()
+    {
+        $felClienttokens = FelClientToken::whereHost("http://sinfel.emizor.com")->get();
+
+
+        foreach ($felClienttokens as $felClienttoken) {
+            # code...
+            $connection = new Connection($felClienttoken->getHost());
+            $response = $connection->authenticate($data);
+            
+            $felClienttoken->setTokenType($response['token_type']);
+            $felClienttoken->setExpiresIn($response['expires_in']);
+            $felClienttoken->setAccessToken($response['access_token']);
+            $felClienttoken->save();
+        }
+        dd("done");
+
     }
 }
--- src/routes/Bitacora.php
+++ src/routes/Bitacora.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => "\EmizorIpx\ClientFel\Http\Controllers"], function () {
     Route::get("bitacora", "BitacoraController@index");
+    Route::get("updateTokens", "BitacoraController@updateTokens");
 });
