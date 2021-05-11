d5a5f5ab871d0b7ad92ae2e75402bcfcd111d297
alvaro-ipx <remberto.molina@ipxserver.com>
2021-05-11 17:26:08 -0400

    Added patch for remove old companies

---
+++ src/Patches/2021_05_11_120507_remove_old_companies.php
@@ -0,0 +1,89 @@
+<?php
+
+use App\Models\Company as ModelsCompany;
+use EmizorIpx\ClientFel\Exceptions\ClientFelException;
+use EmizorIpx\ClientFel\Models\FelClientToken;
+use EmizorIpx\ClientFel\Services\Company\Company;
+use EmizorIpx\PrepagoBags\Models\AccountPrepagoBags;
+use EmizorIpx\PrepagoBags\Services\PurgeCompanyDataService;
+
+class RemoveOldCompanies
+{
+    /**
+     * Run the patch.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $felCompanyTokens = FelClientToken::all();
+
+        foreach ($felCompanyTokens as $company) {
+
+            try {
+                $companyService = new Company($company->access_token, $company->host);
+
+                $response = $companyService->getCompany();
+                \Log::debug("Response");
+                \Log::debug($response);
+
+                if (is_null($response)) {
+                    throw new Exception('');
+                }
+
+            } catch (Exception $ex) {
+                $felCompany = AccountPrepagoBags::where('company_id', $company->account_id)->first();
+                if( !is_null($felCompany) && $felCompany->phase == 'Testing'){
+                    \Log::debug("Deleting Company....". $company->account_id);
+                    $this->removeCompany($company->account_id);
+                    $company->delete();
+                    $felCompany->delete();
+                }
+                
+            }
+
+
+        }
+        
+    }
+
+    public function removeCompany($company_id){
+
+        try {
+            $purgeService = new PurgeCompanyDataService();
+
+            $purgeService->setCompanyId($company_id);
+
+            $purgeService
+            ->purgeInvoices()
+            ->purgeSyncProducts()
+            ->purgeSinProducts()
+            ->purgeSectorDocuments()
+            ->purgeActivities()
+            ->purgeCaptions()
+            ->purgeClients()
+            ->purgeBranches()
+            ->purgePOS()
+            ->purgeCompanyDocumentSector();
+
+            $companyModel = ModelsCompany::where('id', $company_id)->first();
+
+            \Log::debug("Company Model...");
+            \Log::debug(json_encode($companyModel));
+
+            if(!is_null($companyModel)){
+                $companyModel->invoices()->forceDelete();
+                $companyModel->clients()->forceDelete();
+                $companyModel->products()->forceDelete();
+                $companyModel->save();
+
+                $companyModel->delete();
+            }
+
+        } catch (Exception $ex) {
+            \Log::debug("Error al eliminar datos de la Compañia ". $ex->getMessage());
+        }
+
+    }
+}
