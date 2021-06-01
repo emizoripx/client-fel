8e52fd6997d02a370b03e371c0addd7c1af444ba
alvaro-ipx <remberto.molina@ipxserver.com>
2021-06-01 16:22:35 -0400

    Added constants forr sector document types

--- src/Utils/TypeDocumentSector.php
+++ src/Utils/TypeDocumentSector.php
@@ -12,16 +12,33 @@ class TypeDocumentSector
 {
 
     const COMPRA_VENTA = 1;
+    const ALQUILER_BIENES_INMUEBLES = 2;
     const COMERCIAL_EXPORTACION = 3;
+    const COMERCIAL_EXPORTACIÓN_LIBRE_CONSIGNACIÓN = 4;
+    const ZONA_FRANCA = 5;
+    const SERVICIO_TURISTICO_HOSPEDAJE = 6;
+    const COMERCIALIZACIÓN_ALIMENTOS_SEGURIDAD  = 7;
     const TASA_CERO = 8;
+    const COMPRA_VENTA_MONEDA_EXTRANJERA  = 9;
+    const DUTTY_FREE  = 10;
     const SECTORES_EDUCATIVOS = 11;
+    const COMERCIALIZACIÓN_HIDROCARBUROS = 12;
     const SERVICIOS_BASICOS = 13;
+    const PRODUCTOS_ALCANZADOS_ICE = 14;
     const ENTIDADES_FINANCIERAS = 15;
     const HOTELES = 16;
+    const HOSPITALES_CLINICAS = 17;
+    const JUEGOS_AZAR = 18;
+    const HIDROCARBUROS = 19;
     const EXPORTACION_MINERALES = 20;
     const VENTA_INTERNA_MINERALES = 21;
     const TELECOMUNICACIONES = 22;
+    const PREVALORADA = 23;
     const DEBITO_CREDITO = 24;
+    const PRODUCTOS_NACIONALES = 25;
+    const PRODUCTOS_NACIONALES_ICE = 26;
+    const REGIMEN_7RG = 27;
+    const COMERCIAL_EXPORTACIÓN_SERVICIOS = 28;
 
     public static function getInstanceByCode($code):string
     {
@@ -81,21 +98,48 @@ class TypeDocumentSector
             case static::COMPRA_VENTA:
                 return 'FACTURA COMPRA-VENTA';
                 break;
+            case static::ALQUILER_BIENES_INMUEBLES:
+                return 'RECIBO DE ALQUILER DE BIENES INMUEBLES';
+                break;
             
             case static::COMERCIAL_EXPORTACION:
                 return 'FACTURA COMERCIAL DE EXPORTACIÓN';
+                break;
+            case static::COMERCIAL_EXPORTACIÓN_LIBRE_CONSIGNACIÓN:
+                return 'FACTURA COMERCIAL DE EXPORTACIÓN EN LIBRE CONSIGNACIÓN';
+                break;
+            case static::ZONA_FRANCA:
+                return 'FACTURA DE ZONA FRANCA';
+                break;
+            case static::SERVICIO_TURISTICO_HOSPEDAJE:
+                return 'FACTURA DE SERVICIO TURÍSTICO Y HOSPEDAJE';
+                break;
+            case static::COMERCIALIZACIÓN_ALIMENTOS_SEGURIDAD:
+                return 'FACTURA DE COMERCIALIZACIÓN DE ALIMENTOS – SEGURIDAD';
                 break;
             
             case static::TASA_CERO:
                 return 'FACTURA DE TASA CERO POR VENTA DE LIBROS Y TRANSPORTE INTERNACIONAL DE CARGA';
                 break;
+            case static::COMPRA_VENTA_MONEDA_EXTRANJERA:
+                return 'FACTURA DE COMPRA Y VENTA DE MONEDA EXTRANJERA';
+                break;
+            case static::DUTTY_FREE:
+                return 'FACTURA DUTTY FREE';
+                break;
             
             case static::SECTORES_EDUCATIVOS:
                 return 'FACTURA SECTORES EDUCATIVOS';
                 break;
+            case static::COMERCIALIZACIÓN_HIDROCARBUROS:
+                return 'FACTURA DE COMERCIALIZACIÓN DE HIDROCARBUROS';
+                break;
             
             case static::SERVICIOS_BASICOS:
                 return 'FACTURA DE SERVICIOS BÁSICOS';
+                break;
+            case static::PRODUCTOS_ALCANZADOS_ICE:
+                return 'FACTURA PRODUCTOS ALCANZADOS POR EL ICE';
                 break;
             
             case static::ENTIDADES_FINANCIERAS:
@@ -104,6 +148,15 @@ class TypeDocumentSector
             
             case static::HOTELES:
                 return 'FACTURA DE HOTELES';
+                break;
+            case static::HOSPITALES_CLINICAS:
+                return 'FACTURA DE HOSPITALES/CLÍNICAS';
+                break;
+            case static::JUEGOS_AZAR:
+                return 'FACTURA DE JUEGOS DE AZAR';
+                break;
+            case static::HIDROCARBUROS:
+                return 'FACTURA HIDROCARBUROS';
                 break;
             
             case static::EXPORTACION_MINERALES:
@@ -117,8 +170,24 @@ class TypeDocumentSector
             case static::TELECOMUNICACIONES:
                 return 'FACTURA TELECOMUNICACIONES';
                 break;
+            case static::PREVALORADA:
+                return 'FACTURA PREVALORADA';
+                break;
+
             case static::DEBITO_CREDITO:
                 return 'NOTA DÉBITO CRÉDITO';
+                break;
+            case static::PRODUCTOS_NACIONALES:
+                return 'FACTURA DE PRODUCTOS NACIONALES';
+                break;
+            case static::PRODUCTOS_NACIONALES_ICE:
+                return 'FACTURA DE PRODUCTOS NACIONALES - ICE';
+                break;
+            case static::REGIMEN_7RG:
+                return 'FACTURA REGIMEN 7RG';
+                break;
+            case static::COMERCIAL_EXPORTACIÓN_SERVICIOS:
+                return 'FACTURA COMERCIAL DE EXPORTACIÓN DE SERVICIOS';
                 break;
             
             
