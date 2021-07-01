f65a29682ed82bc680921f254945a8f33a6d5e8a
Marco Cuellar <mcuellar@ipxserver.com>
2021-07-01 12:04:13 -0400

    hot fix, payment hashes

--- src/Http/Controllers/BitacoraController.php
+++ src/Http/Controllers/BitacoraController.php
@@ -10,7 +10,8 @@ use EmizorIpx\ClientFel\Repository\FelCredentialRepository;
 use Illuminate\Http\Request;
 use EmizorIpx\ClientFel\Services\Connection\Connection;
 use Exception;
-
+use App\Models\PaymentHash;
+use Hashids\Hashids;
 class BitacoraController extends BaseController
 {
 
@@ -20,6 +21,41 @@ class BitacoraController extends BaseController
     }
     public function index(Request $request)
     {
+        $hashids = new Hashids(config('ninja.hash_salt'), 10);
+        
+        $payments = PaymentHash::get();
+
+        $arr = [];
+        foreach ($payments as $p) {
+            $data = $p->data;
+
+            if ($data->invoices && isset($data->invoices->invoices) ){
+                
+
+                $invoice = $data->invoices->invoices[0];
+                $invoice->invoice_id = $hashids->encode($p->data->invoices->invoices[0]->invoice_id);
+                $data->invoices = $p->data->invoices->invoices;
+                $data->invoices[0] = $invoice;
+
+
+                $total = $data->total;
+                $total->credit_totals = 0;
+                $total->invoice_totals = (float)$total->invoice_totals;
+                $total->fee_total = (float)$total->fee_total;
+                $total->amount_with_fee = (float)$total->amount_with_fee;
+                $data->total = $total;
+                
+                
+                $p->data = $data;
+                $p->save();
+                $arr[] = $data;
+            }
+            
+        }
+        return $arr;
+
+
+
         $logs = BitacoraLog::orderBy("id","desc")->simplePaginate(30);
 
         return view('clientfel::bitacora', compact('logs') );
