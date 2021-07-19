32a91be483186e93ad8f0a43d64df49f974f711e
alvaro-ipx <remberto.molina@ipxserver.com>
2021-07-19 19:10:47 -0400

    Add updated branches

--- src/Http/Controllers/WebhookBranch.php
+++ src/Http/Controllers/WebhookBranch.php
@@ -9,21 +9,22 @@ use Illuminate\Http\Request;
 
 class WebhookBranch extends BaseController
 {
-    public function updateBranch ( Request $request ){
+    public function updateBranch(Request $request)
+    {
 
         $data = $request->get('data');
         $new_branch = $request->get('new_branch');
         \Log::debug("Data");
         \Log::debug($data);
 
-        if($new_branch){
-            $companies =  AccountPrepagoBags::where('fel_company_id', $data['company_id'])->get();
 
-            if($companies){
-                foreach ($companies as $company) {
+        $companies =  AccountPrepagoBags::where('fel_company_id', $data['company_id'])->get();
 
-                    if(!FelBranch::where('codigo', $data['code'])->where('company_id', $company->id)->exists()){
-                        
+        if ($companies) {
+            foreach ($companies as $company) {
+
+                if ($new_branch) {
+                    if (!FelBranch::where('codigo', $data['code'])->where('company_id', $company->id)->exists()) {
                         FelBranch::create([
                             "codigo" => $data['code'],
                             "descripcion" => $data['code'] == 0 ? "Casa Matriz" : "Sucursal " . $data["code"],
@@ -33,15 +34,29 @@ class WebhookBranch extends BaseController
                             "ciudad" => $data['city'],
                             "municipio" => $data['municipalidad']
                         ]);
-                        \Log::debug("Branch Updated");
-                        \Log::debug("Company ID: ". $company->id . " Branch #".$data["code"]);
+                        \Log::debug("Branch created");
+                        \Log::debug("Company ID: " . $company->id . " Branch #" . $data["code"]);
+                    }
+                } else {
+                    \Log::debug("Branch Updated");
+                    $branch = FelBranch::where('codigo', $data['code'])->where('company_id', $company->id)->first();
+
+                    if($branch){
+                        $branch->codigo = $data['code'];
+                        $branch->descripcion = $data['code'] == 0 ? "Casa Matriz" : "Sucursal " . $data["code"];
+                        $branch->company_id = $company->id;
+                        $branch->zona = $data['zone'];
+                        $branch->pais = $data['country'];
+                        $branch->ciudad = $data['city'];
+                        $branch->municipio = $data['municipalidad'];
+                        $branch->save();
+
+                        \Log::debug("Se actualizó la Surcursal #" . $data['code']);
                     }
                 }
-            } else {
-                \Log::debug("No exite la compañia");
             }
+        } else {
+            \Log::debug("No exite la compañia");
         }
-        
-
     }
 }
