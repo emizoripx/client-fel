42dc42366c9853c86dd054863de4ca69e2a42b43
alvaro-ipx <remberto.molina@ipxserver.com>
2021-07-20 11:06:15 -0400

    Fixed Webhook

--- src/Http/Controllers/WebhookBranch.php
+++ src/Http/Controllers/WebhookBranch.php
@@ -22,9 +22,9 @@ class WebhookBranch extends BaseController
 
         if ($companies) {
             foreach ($companies as $company) {
-
-                if ($new_branch) {
-                    if (!FelBranch::where('codigo', $data['code'])->where('company_id', $company->id)->exists()) {
+                
+                $branch = FelBranch::where('codigo', $data['code'])->where('company_id', $company->id)->first();
+                if (empty($branch)) {
                         FelBranch::create([
                             "codigo" => $data['code'],
                             "descripcion" => $data['code'] == 0 ? "Casa Matriz" : "Sucursal " . $data["code"],
@@ -36,10 +36,9 @@ class WebhookBranch extends BaseController
                         ]);
 
                         \Log::debug("Branch created");
-                        // sync sector document type
-                        \Log::debug("Sector Document");
-                        $fel_sector_document_types = \DB::table('fel_sector_document_types')->whereCompanyId($company->id)->whereCodigoSucursal(0)->get();
 
+                        // sync sector document type
+                        $fel_sector_document_types = \DB::table('fel_sector_document_types')->whereCompanyId($company->id)->where('codigoSucursal', 0)->get();
                         foreach ($fel_sector_document_types as $fel_sector_document_type) {
                             
                             \DB::table('fel_sector_document_types')->insert([
@@ -47,16 +46,14 @@ class WebhookBranch extends BaseController
                                 "codigoSucursal" => $data['code'],
                                 "documentoSector" => $fel_sector_document_type->documentoSector,
                                 "tipoFactura" => $fel_sector_document_type->tipoFactura,
-                                "codigo" => $fel_sector_document_type->codigo,
+                                "codigo" => $fel_sector_document_type->codigo
                             ]);
                         }
                         \Log::debug("fel_sector_document_types assigned");
                        
                         \Log::debug("Company ID: " . $company->id . " Branch #" . $data["code"]);
-                    }
                 } else {
                     \Log::debug("Branch Updated");
-                    $branch = FelBranch::where('codigo', $data['code'])->where('company_id', $company->id)->first();
 
                     if($branch){
                         $branch->codigo = $data['code'];
