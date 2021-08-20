328687151c5f9cf00f65e8951c7012349531077e
Marco Cuellar <mcuellar@ipxserver.com>
2021-08-20 19:19:58 -0400

    - version 1.9.29 ,hot fix, variable misstype

--- src/Models/FelParametric.php
+++ src/Models/FelParametric.php
@@ -122,7 +122,7 @@ class FelParametric
 
             $d["updated_at"] = Carbon::now()->toDateTimeString();
             $d["created_at"] = Carbon::now()->toDateTimeString();
-            unset($d["is_active"]);
+            unset($d["isActive"]);
             $data_added[] = $d;
         }
         \Log::debug($data_added);
