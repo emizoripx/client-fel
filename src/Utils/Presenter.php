c826b4edae77c23cba4654be084da2bb6a5c528f
Marco Cuellar <mcuellar@ipxserver.com>
2021-11-08 11:05:05 -0400

    version 2.3.12

--- src/Utils/Presenter.php
+++ src/Utils/Presenter.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace EmizorIpx\ClientFel\Utils;
 
 use EmizorIpx\ClientFel\Http\Resources\InvoiceResource;
@@ -9,23 +10,39 @@ use EmizorIpx\ClientFel\Models\FelParametric;
 use EmizorIpx\ClientFel\Models\FelSyncProduct;
 use stdClass;
 
-class Presenter {
-
-    
-
-    public static function appendFelData($data, $company_id) : array
+class Presenter
+{
+    public static function appendFelData($data, $company_id): array
     {
         $types = TypeParametrics::getAll();
 
         $parametrics = new stdClass;
 
-        foreach ( $types as $type) {
+        foreach ($types as $type) {
             //TODO: cached tables 
-            $parametrics->{$type} = FelParametric::index($type, $company_id);
+            if ($company_id == 165 && $type == "tipos-documento-sector") {
+                $tipos_documento_sector = FelParametric::index($type, $company_id);
+
+                foreach ($tipos_documento_sector as $key => $value) {
+                    if ($value->codigo == 1) {
+
+                        $value->documentoSector = "FACTURA SECTOR-EDUCATIVO";
+
+                        $tipos_documento_sector[$key] = $value;
+                    } else {
+                        $tipos_documento_sector[$key] = $value;
+                    }
+                }
+                $parametrics->{$type} = $tipos_documento_sector;
+            } else {
+                $parametrics->{$type} = FelParametric::index($type, $company_id);
+            }
         }
 
+
+
         $data["fel_data"]["parametrics"] = $parametrics;
-        
+
         // $data["fel_data"]["invoices"] = InvoiceResource::collection( FelInvoiceRequest::getByCompanyId($company_id) );
 
         // $data["fel_data"]["products"] = ProductResource::collection(FelSyncProduct::getByCompanyId($company_id));
@@ -33,8 +50,5 @@ class Presenter {
         // $data["fel_data"]["clients"] = FelClient::getByCompanyId($company_id);
 
         return $data;
-
     }
-
-    
 }
