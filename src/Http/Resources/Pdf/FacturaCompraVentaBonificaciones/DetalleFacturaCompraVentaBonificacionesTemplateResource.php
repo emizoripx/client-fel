132ed936ed3f4274c40e6f215e97ca3d886d7224
Marco Cuellar <mcuellar@ipxserver.com>
2022-05-24 09:14:51 -0400

    added resource bonificaciones

---
+++ src/Http/Resources/Pdf/FacturaCompraVentaBonificaciones/DetalleFacturaCompraVentaBonificacionesTemplateResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace EmizorIpx\ClientFel\Http\Resources\Pdf\FacturaCompraVentaBonificaciones;
+
+use EmizorIpx\ClientFel\Http\Resources\Pdf\BaseDetalleTemplateResource;
+
+class DetalleFacturaCompraVentaBonificacionesTemplateResource extends BaseDetalleTemplateResource {
+
+    public function toArray($request)
+    {
+        $common = parent::toArray($request);
+        $common['cantidad'] = NumberUtils::number_format_custom($this->cantidad, 5);
+        $common['precioUnitario'] = intval($this->precioUnitario) < $this->precioUnitario ? NumberUtils::number_format_custom((float) $this->precioUnitario, 5) : NumberUtils::number_format_custom((float) $this->precioUnitario, 5);
+        $common["subTotal"] = NumberUtils::number_format_custom((float) $this->subTotal, 5);
+
+        return $common;
+        
+    }
+
+}
