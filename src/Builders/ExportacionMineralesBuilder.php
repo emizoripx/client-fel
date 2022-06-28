8edbb7df166a2b0a4c8fb8b8f7c6561ecd999eb3
alvaro-ipx <remberto.molina@ipxserver.com>
2022-06-28 16:36:09 -0400

    Minor Fixes

--- src/Builders/ExportacionMineralesBuilder.php
+++ src/Builders/ExportacionMineralesBuilder.php
@@ -114,7 +114,7 @@ class ExportacionMineralesBuilder extends BaseFelInvoiceBuilder implements FelIn
             $new->descripcionLeyes = !empty($detail->leyes) ? $detail->leyes."" : "";
             $new->codigoNandina = $detail->codigoNandina;
             
-            if( !empty($detail->valorBruto) || !is_null($detail->valorBruto) ) {
+            if( isset($detail->valorBruto) || !is_null($detail->valorBruto) ) {
 
                 $new->valorBruto = $detail->valorBruto;
             }
