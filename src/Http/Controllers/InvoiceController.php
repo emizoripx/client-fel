ca91a0dfb473efe893564f6e94e669a1f19c706c
Marco Cuellar <mcuellar@ipxserver.com>
2022-10-04 16:42:06 -0400

    adjutsment

--- src/Http/Controllers/InvoiceController.php
+++ src/Http/Controllers/InvoiceController.php
@@ -66,7 +66,11 @@ class InvoiceController extends BaseController
 
             event(new InvoiceWasEmited($felInvoiceRequest->invoice_origin(), $felInvoiceRequest->invoice_origin()->company, Ninja::eventVars(auth()->user()->id) ));
             \Log::debug("################################################## FLUJO ============================================ FACTURA EMITIDA");
-            return redirect('api/v1/invoices/'. $request->input('id_origin'));
+            // return redirect('api/v1/invoices/'. $request->input('id_origin'));
+            return response()->json([
+                'success' => true
+            ]);
+
         } catch (ClientFelException $ex) {
             
             fel_register_historial($felInvoiceRequest, $ex->getMessage());
@@ -96,14 +100,14 @@ class InvoiceController extends BaseController
             return response()->json([
                 'success' => true
             ]);
-            // return redirect('api/v1/invoices/' . $request->input('id_origin'));
+            
 
         } catch(ClientFelException $ex){
             return response()->json([
                 'success' => false,
                 'msg' => $ex->getMessage()
             ]);
-            // return redirect('api/v1/invoices/' . $request->input('id_origin'));
+
         }
     }
     public function reversionRevocate(Request $request){
