fd634e3ee9253000412a02ae4c5001c62dababd5
Marco Cuellar <mcuellar@ipxserver.com>
2023-07-10 17:10:18 -0400

    change daily report universidad la paz

--- src/Reports/Invoices/ComprobanteDiarioUnslpReport.php
+++ src/Reports/Invoices/ComprobanteDiarioUnslpReport.php
@@ -38,20 +38,32 @@ class ComprobanteDiarioUnslpReport extends BaseReport implements ReportInterface
 
     public function generateReport()
     {
-        
-        $query_invoices = \DB::table('fel_invoice_requests')
-                        ->leftJoin("invoices",'fel_invoice_requests.id_origin','invoices.id')
-                        ->leftJoin("clients",'invoices.client_id','clients.id')
-                        ->leftJoin("group_settings", 'group_settings.id', 'clients.group_settings_id')
-                        ->where('fel_invoice_requests.company_id', $this->company_id)
-                        ->whereNotNull('fel_invoice_requests.codigoEstado')  
-                        ->whereNotNull('fel_invoice_requests.cuf');  
 
-        $query_invoices = $this->addDateFilter($query_invoices);
+        $query_base = \DB::table('fel_invoice_requests')
+        ->where('fel_invoice_requests.company_id', $this->company_id)
+            ->where(function ($query) {
+                $query->whereNull('fel_invoice_requests.codigoEstado')
+                ->orWhere('fel_invoice_requests.codigoEstado', '!=', 902);
+            });
+
+        $query_base = $this->addDateFilter($query_base);
+        $query_base = $query_base->select('fel_invoice_requests.id', 'fel_invoice_requests.numeroFactura', 'fel_invoice_requests.fechaEmision');
+
+
+        $query_invoices = $query_invoices
+            ->mergeBindings($query_base)
+            ->join(
+                \DB::raw('(' . $query_base->toSql() . ') pr'),
+                function ($join) {
+                    $join->on('fel_invoice_requests.id', '=', 'pr.id');
+                }
+            )
+            ->leftJoin("invoices", 'fel_invoice_requests.id_origin', 'invoices.id')
+            ->leftJoin("clients", 'invoices.client_id', 'clients.id')
+            ->leftJoin("group_settings", 'group_settings.id', 'clients.group_settings_id')
+            ->whereNotNull('fel_invoice_requests.estado');
+
         $query_invoices =  $this->addSelectColumns($query_invoices);
-
-      
-
         return [
             "header" => $this->headers_csv,
             "invoices" => $query_invoices
