99019ab22f218f5d49090b249a19851935c9fcb6
Marco Cuellar <mcuellar@emizor.com>
2024-02-26 18:14:40 -0400

    fix, migration failed

--- src/database/migrations/2024_02_06_184223_add_columns_receipts_invoices_table.php
+++ src/database/migrations/2024_02_06_184223_add_columns_receipts_invoices_table.php
@@ -14,11 +14,21 @@ class AddColumnsReceiptsInvoicesTable extends Migration
     public function up()
     {
         Schema::table('invoices', function (Blueprint $table) {
-            $table->json("document_data")->nullable();
-            $table->string("document_search")->nullable()->index();
-            $table->enum('document_type', ["receipt", "invoice"])->default("invoice");
-            $table->index(["company_id", "document_type"]);
-            $table->index(["company_id", "document_type", "created_at"]);
+            if (!Schema::hasColumn('invoices', 'document_data')) {
+                $table->json("document_data")->nullable();
+            }
+            if (!Schema::hasColumn('invoices', 'document_search')) {
+                $table->string("document_search")->nullable()->index();
+            }
+            if (!Schema::hasColumn('invoices', 'document_type')) {
+                $table->enum('document_type', ["receipt", "invoice"])->default("invoice");
+            }
+            if (!Schema::hasColumn('invoices', 'company_id') || !Schema::hasColumn('invoices', 'document_type')) {
+                $table->index(["company_id", "document_type"]);
+            }
+            if (!Schema::hasColumn('invoices', 'company_id') || !Schema::hasColumn('invoices', 'document_type') || !Schema::hasColumn('invoices', 'created_at')) {
+                $table->index(["company_id", "document_type", "created_at"]);
+            }
         });
     }
 
