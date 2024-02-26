531c8248d5c23e21314ab40f9a9a860f7ec14238
Marco Cuellar <mcuellar@emizor.com>
2024-02-26 16:30:52 -0400

    fix v2.14

--- src/database/migrations/2024_02_06_184223_add_columns_receipts_invoices_table.php
+++ src/database/migrations/2024_02_06_184223_add_columns_receipts_invoices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnsReceiptsInvoices extends Migration
+class AddColumnsReceiptsInvoicesTable extends Migration
 {
     /**
      * Run the migrations.
