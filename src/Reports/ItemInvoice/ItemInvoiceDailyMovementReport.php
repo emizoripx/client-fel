0618fe89aa45a06ebc23202ca54509add24c1134
Marco Cuellar <mcuellar@emizor.com>
2024-03-15 06:37:32 -0400

    fix reporte MOVIMIENDO DIARIO, remove canceled payments

--- src/Reports/ItemInvoice/ItemInvoiceDailyMovementReport.php
+++ src/Reports/ItemInvoice/ItemInvoiceDailyMovementReport.php
@@ -102,6 +102,7 @@ class ItemInvoiceDailyMovementReport extends BaseReport implements ReportInterfa
         ->leftJoin('payment_types', 'payments.type_id', 'payment_types.id')
         ->where('fel_invoice_requests.company_id', $this->company_id)
         ->whereNotNull('fel_invoice_requests.cuf')
+        ->whereNull('paymentables.deleted_at')
         ->whereBetween('fel_invoice_requests.fechaEmision', [$from, $to])
         ->whereNotBetween('paymentables.created_at', [$from, $to])
         ->selectRaw(\DB::raw('fel_invoice_requests.id, codigoEstado, fel_invoice_requests.fechaEmision,fel_invoice_requests.numeroFactura, if(fel_invoice_requests.codigoEstado =691 or fel_invoice_requests.codigoEstado = 905, "ANULADO", if(paymentables.created_at is null,"Por cobrar","PAGADO") ) AS estado, fel_invoice_requests.codigoCliente,fel_invoice_requests.numeroDocumento, fel_invoice_requests.nombreRazonSocial, payment_types.name as tipoPago, paymentables.created_at as fechaPago, fel_invoice_requests.detalles, fel_invoice_requests.usuario,fel_invoice_requests.montoTotal,fel_invoice_requests.descuentoAdicional, JSON_EXTRACT(extras,"$.poliza") as poliza, JSON_EXTRACT(extras,"$.agencia") as agencia'));
@@ -112,6 +113,7 @@ class ItemInvoiceDailyMovementReport extends BaseReport implements ReportInterfa
         ->leftJoin('payments', 'payments.id', 'paymentables.payment_id')
         ->leftJoin('payment_types', 'payments.type_id', 'payment_types.id')
         ->where('fel_invoice_requests.company_id', $this->company_id)
+            ->whereNull('paymentables.deleted_at')
             ->whereNotNull('fel_invoice_requests.cuf')
             ->whereBetween('fel_invoice_requests.fechaEmision', [$from, $to])
             ->whereBetween('paymentables.created_at', [$from, $to])
@@ -123,6 +125,7 @@ class ItemInvoiceDailyMovementReport extends BaseReport implements ReportInterfa
         ->leftJoin('payments', 'payments.id', 'paymentables.payment_id')
         ->leftJoin('payment_types', 'payments.type_id', 'payment_types.id')
         ->where('fel_invoice_requests.company_id', $this->company_id)
+            ->whereNull('paymentables.deleted_at')
             ->whereNotNull('fel_invoice_requests.cuf')
             ->whereNotBetween('fel_invoice_requests.fechaEmision', [$from, $to])
             ->whereBetween('paymentables.created_at', [$from, $to])
